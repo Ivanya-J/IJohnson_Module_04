10d0a3ee6fdc81e647fc0146badb40a1103d6b1b
Ivanya-J <ivanya.johnson@my.jcu.edu.au>
2024-09-09 00:40:52 +1000

    Continuation of Module 4 and the upcoming Assignments

--- Module 04_Johnson.R
+++ Module 04_Johnson.R
@@ -2,7 +2,7 @@
 
 ## Getting Gib Set Up ####
 
- gitcreds::gitcreds_set()
+# gitcreds::gitcreds_set()
 # install.packages("tidyverse")
 library(tidyverse)
 
@@ -160,18 +160,210 @@ ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy), position = "j
 
 ## Labels ####
 
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(color = class)) +
+  geom_smooth(se = FALSE) +
+  labs(title = "Fuel efficiency generally decreases with engine size")
+
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(color = class)) +
+  geom_smooth(se = FALSE) +
+  labs(
+    title = "Fuel efficiency generally decreases with engine size",
+    subtitle = "Two seaters (sports cars) are an exception because of their light weight",
+    caption = "Data from fueleconomy.gov"
+  )
+
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(color = class)) +
+  geom_smooth(se = FALSE) +
+  labs(
+    x = "Engine displacement (L)",
+    y = "Highway fuel economy (mpg)",
+    colour = "Car type"
+  )
+
 ## Annotations ####
+
+best_in_class <- mpg %>%
+  group_by(class) %>%
+  filter(row_number(desc(hwy)) == 1)
+
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(color = class)) +
+  geom_text(aes(label = model), data = best_in_class)
 
 ## Scales ####
 
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(color = class))
+
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(color = class)) +
+  scale_x_continuous() +
+  scale_y_continuous() +
+  scale_colour_discrete()
+
 ## Axis Ticks ####
+
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point() +
+  scale_y_continuous(breaks = seq(15, 40, by = 5))
+# Sets the y-axis range (min: 15, max: 40) and goes in increments of 5
+
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point() +
+  scale_x_continuous(labels = NULL) +
+  scale_y_continuous(labels = NULL)
 
 ## Legends and Color Schemes ####
 
+base <- ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(color = class))
+
+base + theme(legend.position = "left")
+base + theme(legend.position = "top")
+base + theme(legend.position = "bottom")
+base + theme(legend.position = "right") # the default
+base + theme(legend.position = "none")
+
 ## Replacing a Scale ####
+
+ggplot(diamonds, aes(carat, price)) +
+  geom_bin2d() + 
+  scale_x_log10() + 
+  scale_y_log10()
+
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(color = drv))
+
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(color = drv)) +
+  scale_color_brewer(palette = "Set1")
+
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(color = drv, shape = drv)) +
+  scale_color_brewer(palette = "Set1")
+
+presidential %>%
+  mutate(id = 33 + row_number()) %>%
+  ggplot(aes(start, id, color = party)) +
+  geom_point() +
+  geom_segment(aes(xend = end, yend = id)) +
+  scale_color_manual(values = c(Republican = "red", Democratic = "blue"))
+
+## Popular Color Package ####
+
+# install.packages("viridis")
+# install.packages("hexbin")
+library(viridis)
+library(hexbin)
+
+df <- tibble(
+  x = rnorm(10000),
+  y = rnorm(10000)
+) # Making a fake data set to plot using this function
+
+ggplot(df, aes(x, y)) +
+  geom_hex() + # New geom!
+  coord_fixed()
+
+ggplot(df, aes(x, y)) +
+  geom_hex() +
+  viridis::scale_fill_viridis() +
+  coord_fixed()
 
 ## Themes ####
 
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(color = class)) +
+  geom_smooth(se = FALSE) +
+  theme_bw()
+
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(color = class)) +
+  geom_smooth(se = FALSE) +
+  theme_light()
+
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(color = class)) +
+  geom_smooth(se = FALSE) +
+  theme_classic()
+
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(color = class)) +
+  geom_smooth(se = FALSE) +
+  theme_dark()
+
+# Theme that Ben wrote himself so his plots are the same style. We can do this to format our own as well
+
+theme (panel.border = element_blank(),
+       panel.grid.minor.x = element_blank(),
+       panel.grid.minor.y = element_blank(),
+       legend.position = "bottom",
+       legend.title = element_blank(),
+       legend.text = element_text(size=8),
+       panel.grid.major = element_blank(),
+       legend.key = element_blank(),
+       legend.background = element_blank(),
+       axis.text.y = element_text(colour="black"),
+       axis.text.x = element_text(colour="black"),
+       text = element_text(family="Arial")) 
+
 ## Saving/Exporting Plots ####
 
+ggplot(mpg, aes(displ, hwy)) + geom_point()
+
+ggsave("Mod4-Workshop2-plot.pdf")
+
+# Use width width and height arguments to play with dimensions of your plot
+
 ## Workshop 3 - Data Wrangling ####
+
+## Tidy Data ####
+
+##  Exercise 3 ####
+
+## Pivoting Data ####
+
+## Lengthening Data sets ####
+
+## Pivoting Longer ####
+
+## Widening Data sets ####
+
+## Pivoting Wider ####
+
+## Exercise 4 ####
+
+## Separating/Uniting Data Tables ####
+
+## Handling Missing Values ####
+
+## Explicit missing values ####
+
+## Fixed Values ####
+
+## NaN ####
+
+# NaN = Not a Number
+
+## Implicit Missing Values ####
+
+## Import Data into R ####
+
+## CSV Files ####
+
+## Practical Advice ####
+
+## Exercise 5 ####
+
+## Relational Data ####
+
+## Joining Data Sets ####
+
+## Mutating Joins ####
+
+## Pipes for Readable Workflows ####
+
+## Workshop 4 - Spatial Data in R ####
